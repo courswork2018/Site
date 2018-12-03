375cd21ddb8efbd53cca57cffebf9454a2c70b44
Katya Grohotova <katya.grohotova.4@gmail.com>
2018-12-04 01:48:24 +0700

    new test base)))

--- sentifilm/test_RNN.py
+++ sentifilm/test_RNN.py
@@ -12,17 +12,17 @@ class Test_RNN(unittest.TestCase):
         list = cursor.fetchall()
         k = 0
         for i in list:
-            # score = views.text(i[0])
-            score = views.text(i[1])
+            score = views.text(i[0])
+            # score = views.text(i[1])
             score = round(float(score))
-            # if score == int(i[1]):
-            if score == int(i[2]):
+            if score == int(i[1]):
+            # if score == int(i[2]):
                 k += 1
         self.assertGreater(round(k / len(list) * 100), 60)
 
-    # def test_spase(self):
-    #     self.assertRaises(BaseException, sentifilm.views.work_rnn, " ", loaded_model) #(exs, fun, *args, **kwds) — fun(*args, **kwds) порождает исключение exc
-    #
+    def test_spase(self):
+        self.assertRaises(BaseException, views.text, "") #(exs, fun, *args, **kwds) — fun(*args, **kwds) порождает исключение exc
+
     # def test_enter(self):
     #     self.assertRaises(BaseException, sentifilm.views.work_rnn, "\n", loaded_model)
     #
