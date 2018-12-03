e0a6596db12e85b661e69a06661830ffcef5ca91
courswork2018 <maryska-98@mail.ru>
2018-12-03 20:21:57 +0300

    +text

--- sentifilm/views.py
+++ sentifilm/views.py
@@ -19,7 +19,7 @@ def about (request):
 def document(request):
     return render(request, 'document.html')
 
-def predict_sentiment(text):
+def predict_sentiment(fulltext):
     # with open('mnist_model.h5',encoding='cp251') as jsonfile:
     #     json_file = json.load(jsonfile)
  #  Загружаем данные об архитектуре сети
@@ -29,15 +29,19 @@ def predict_sentiment(text):
     # Создаем модель
     loaded_model = model_from_yaml(loaded_model_yaml)
     # Загружаем сохраненные веса в модель
-    loaded_model.load_weights('mnist_model.h5')
+    loaded_model.load_weights('model.h5')
 
     max_review_length = 250
-
+    text=numpy.array([fulltext])
     tk = keras.preprocessing.text.Tokenizer(num_words=None, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~ ', lower=True,
                                             split=' ', char_level=False, oov_token=None, document_count=0)
     tk.fit_on_texts(text)
     score = loaded_model.predict(sequence.pad_sequences(tk.texts_to_sequences(text), maxlen=max_review_length))
     K.clear_session()
+    return score
+
+def text(fulltext):
+    score = predict_sentiment(fulltext)
     return score
 
 def analysis(request):
@@ -46,11 +50,11 @@ def analysis(request):
     score = predict_sentiment(fulltext)
 
 
-    if max(score) > 0.5:
+    if score > 0.5:
         sentiment = "позитивным"
     else:
         sentiment = "негативным"
 
-    return render(request, 'analysis.html', {'sentiment': sentiment, 'score': max(score)})
+    return render(request, 'analysis.html', {'sentiment': sentiment, 'score': score})
 
 
