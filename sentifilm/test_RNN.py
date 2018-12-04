58f968f4dcd9440bb3c1b814cf0c954f7d16d6aa
Katya Grohotova <katya.grohotova.4@gmail.com>
2018-12-04 13:23:45 +0700

    test_time

--- sentifilm/test_RNN.py
+++ sentifilm/test_RNN.py
@@ -1,6 +1,7 @@
 import unittest
 import views
 import sqlite3
+import time
 
 class Test_RNN(unittest.TestCase):
 
@@ -10,12 +11,17 @@ class Test_RNN(unittest.TestCase):
         cursor.execute("SELECT * FROM test_base11")
         list = cursor.fetchall()
         k = 0
+        global time11
+        time11 = 0
         for i in list:
+            start_time = time.time()
             score = views.text(i[0])
+            time11 = time11 + (time.time() - start_time)
             score = round(float(score))
             if score == int(i[1]):
                 k += 1
-        self.assertGreater(round(k / len(list) * 100), 60)
+        time11 = time11 / len(list)
+        self.assertGreaterEqual(round(k / len(list) * 100), 60)
 
     def test_rnn_base(self):
         conn = sqlite3.connect("../test_base/test_base.db")
@@ -23,12 +29,23 @@ class Test_RNN(unittest.TestCase):
         cursor.execute("SELECT * FROM test_base")
         list = cursor.fetchall()[0:700:2]
         k = 0
+        global time_
+        time_ = 0
         for i in list:
+            start_time = time.time()
             score = views.text(i[1])
+            time_ = time_ + (time.time() - start_time)
             score = round(float(score))
             if score == int(i[2]):
                 k += 1
-        self.assertGreater(round(k / len(list) * 100), 60)
+        time_ = time_ / len(list)
+        self.assertGreaterEqual(round(k / len(list) * 100), 60)
+
+    def test_time(self):
+        # print(time.time())
+        res_time = (time11 + time_) / 2
+        self.assertLess(res_time, 0.3)
+
 
     # def test_spase(self):
     #     self.assertRaises(BaseException, views.text, "") #(exs, fun, *args, **kwds) — fun(*args, **kwds) порождает исключение exc
